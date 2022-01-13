d16c1dd26f89da4f7de188934df5fbbb2f5a837d
Rohitash Mathur <rohitashmathur@Rohitashs-MacBook-Pro.local>
2022-01-14 03:33:13 +0530

    Added time feature

--- Bridge.js
+++ Bridge.js
@@ -1,8 +1,11 @@
 function AppDetailsBridge(){
+    var AppDetailBridgeTimeStart = new Date().getTime();
     window.addEventListener('onAppDetailsReceived', (event, data) => {
         AppDetailresponse = event.detail;
+        var AppDetailBridgeTimeElapsed = new Date().getTime() - AppDetailBridgeTimeStart;
         console.log(AppDetailresponse);
         document.getElementById("output").innerHTML = JSON.stringify(AppDetailresponse,null,2);
+        document.getElementById("ttl").innerHTML = AppDetailBridgeTimeElapsed;
     });
     try {
         if (window.webkit) {
@@ -16,10 +19,13 @@ function AppDetailsBridge(){
 }
 
 function AuthenticationBridge(){
+    var AuthenticationBridgeTimeStart = new Date().getTime();
     window.addEventListener('onAuthenticationDetailsReceived', (event, data) => {
         Authresponse = event.detail;
+        var AuthenticationBridgeTimeElapsed = new Date().getTime() - AuthenticationBridgeTimeStart;
         console.log(Authresponse);
         document.getElementById("output").innerHTML = JSON.stringify(Authresponse,null,2);
+        document.getElementById("ttl").innerHTML = AuthenticationBridgeTimeElapsed;
     });
     try {
         if (window.webkit) {
