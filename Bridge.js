6ed7640f5bc1ff743e140bd7eace11544e79ad4f
Naufal Hafiz <naufal.hafiz@tiket.com>
2024-12-19 18:53:46 +0700

    added log

--- Bridge.js
+++ Bridge.js
@@ -132,6 +132,7 @@ export function CallHandleActionSendContent(){
                 }
             }
         }
+        console.log(ReqJSON)
         
         if (window.webkit) {
             // window?.webkit?.messageHandlers?.callGenericNativeJSI?.postMessage?.(JSON.stringify(ReqJSON))
